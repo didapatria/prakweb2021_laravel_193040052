dfa9667de66915a119237420e11815a78584df9d
didapatria <didapatria3@gmail.com>
2021-10-07 11:02:25 +0700

    Menyelesaikan video Belajar Laravel 8 | 3. Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Adinda Fadhil Patria",
+        "email" => "didapatria3@gmail.com",
+        "image" => "dida.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
