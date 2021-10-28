e43ceebe0665c8f1d869cffe0cfe7811e342d0f2
didapatria <didapatria3@gmail.com>
2021-10-28 12:47:56 +0700

    Menyelesaikan video Belajar Laravel 8 | 15. User Registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,9 +3,17 @@
 @section('container')
 <div class="row justify-content-center">
   <div class="col-md-4">
+
+    @if (session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
     <main class="form-signin">
       <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-      <form>    
+      <form>
         <div class="form-floating">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
           <label for="floatingInput">Email address</label>
